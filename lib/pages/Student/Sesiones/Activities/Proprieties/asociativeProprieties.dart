fceb81f84384e9fd1153530a689077df785360fa
Carlos Baquero <karlosmario0123@gmail.com>
2021-04-19 22:14:21 -0500

    corrección de refactorización

--- lib/pages/Student/Sesiones/Activities/Proprieties/asociativeProprieties.dart
+++ lib/pages/Student/Sesiones/Activities/Proprieties/asociativeProprieties.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 import 'package:discalapp_proy/pages/Student/Sesiones/baseActivity.dart';
+import 'package:discalapp_proy/shared/ActivityFrame.dart';
 import 'package:flutter/material.dart';
 
 class AsociativeProp extends StatefulWidget {
@@ -19,39 +20,7 @@ class AsociativePropietState extends BaseActivity<AsociativeProp> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Container(
-      margin: EdgeInsets.only(top: 10, bottom: 20, left: 20, right: 20),
-      padding: EdgeInsets.only(bottom: 20),
-      width: double.infinity,
-      child: Column(
-        children: [
-          Container(
-            margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-            child: Text("Actividad",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    fontSize: 22,
-                    color: Colors.blue[700],
-                    fontWeight: FontWeight.w500)),
-          ),
-          // operacionText(),
-          //respuestaInput()
-        ],
-      ),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.all(Radius.circular(10)),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.blue[900].withOpacity(0.5),
-            spreadRadius: 5,
-            blurRadius: 7,
-            offset: Offset(2, 5), // changes position of shadow
-          ),
-        ],
-      ),
-    ));
+    return marcoActividad("Actividad",[]);
   }
 
   _generarNumeros() {
