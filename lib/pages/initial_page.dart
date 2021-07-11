be9006b64325f379cf4cc322efdbf3f7cd1ef70d
Carlos Baquero <karlosmario0123@gmail.com>
2021-07-10 21:23:25 -0500

    cambio diseño inicio

--- lib/pages/initial_page.dart
+++ lib/pages/initial_page.dart
@@ -9,33 +9,26 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class InitialPage extends StatefulWidget {
-  InitialPage({ Key? key}) : super(key: key);
+  InitialPage({Key? key}) : super(key: key);
 
   @override
   _InitialPageState createState() => _InitialPageState();
 }
 
 class _InitialPageState extends State<InitialPage> {
-
-  
   bool intentandoLoguear = false;
   bool buscando = true;
-  ButtonStyle style = ElevatedButton.styleFrom(
-      primary: kTeacherColor,
-      elevation: 20,
-      shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(20))));
   double _initLogoPos = 0.61;
-  double _initBottomPos = 0.45;
-  double _finalLogoPos = 0.15;
-  double _finalBottomPos = 0;
+  double _finalLogoPos = 0.12;
   double _logoPos = 0.61;
-  double _bottomPos = 0.45;
   double _buttonsOpacity = 1.0;
   double _backButtonOpacity = 0.0;
   double _menuOpacity = 0.0;
   double _menuPosition = -1;
 
+  String _logoBlanco = 'assets/images/Discalpp-LogoBlanco.png';
+  String _logoNormal = 'assets/images/Discalpp-text1.png';
+  String _logoActual = 'assets/images/Discalpp-LogoBlanco.png';
   String _menuLabel = '';
 
   bool _isLogin = false;
@@ -51,11 +44,9 @@ class _InitialPageState extends State<InitialPage> {
     super.initState();
     buscarUsuario();
   }
+
   @override
   Widget build(BuildContext context) {
-    
-    
-
     Size size = MediaQuery.of(context).size;
 
     return Scaffold(
@@ -68,7 +59,7 @@ class _InitialPageState extends State<InitialPage> {
             ),
             AnimatedPositioned(
               duration: Duration(milliseconds: 300),
-              top: size.height * _bottomPos,
+              top: size.height * 0.45,
               child: bottomImage(size),
             ),
             AnimatedPositioned(
@@ -86,10 +77,10 @@ class _InitialPageState extends State<InitialPage> {
             ),
             Positioned(bottom: 20, left: 20, child: backButton()),
             Positioned(
-              top: size.height * 0.3,
+              top: size.height * 0.20,
               right: size.width * _menuPosition,
               child: loginOptions(size),
-            )
+            ),
           ],
         ));
   }
@@ -102,13 +93,6 @@ class _InitialPageState extends State<InitialPage> {
         width: size.width,
         child: Column(
           children: [
-            Text(
-              _menuLabel,
-              style: TextStyle(
-                  fontSize: 25,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white),
-            ),
             SizedBox(height: 40),
             optionAlumno(),
             SizedBox(height: 20),
@@ -121,7 +105,11 @@ class _InitialPageState extends State<InitialPage> {
 
   Widget optionDocente() {
     return ElevatedButton(
-      style: style,
+      style: ElevatedButton.styleFrom(
+          primary: kAlumnColor,
+          elevation: 20,
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(20)))),
       onPressed: () {
         if (_isLogin) {
           Navigator.pushNamed(context, 'loginTeacher');
@@ -129,20 +117,24 @@ class _InitialPageState extends State<InitialPage> {
           Navigator.pushNamed(context, 'registerTeacher');
         }
       },
-      child: Column(
-        children: [
-          Text(
-            'Docente',
-            style: TextStyle(
-              fontSize: 20,
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
+      child: Container(
+        padding: EdgeInsets.only(top: 10, left: 10, right: 10),
+        child: Column(
+          children: [
+            Text(
+              _menuLabel + '\nDocente',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          ),
-          Image(
-              height: 150,
-              image: AssetImage('assets/images/iconoProfesor.png')),
-        ],
+            Image(
+                height: 150,
+                image: AssetImage('assets/images/iconoProfesor.png')),
+          ],
+        ),
       ),
     );
   }
@@ -152,28 +144,32 @@ class _InitialPageState extends State<InitialPage> {
       style: ElevatedButton.styleFrom(
         primary: kAlumnColor,
         shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(Radius.circular(20))),
+            borderRadius: BorderRadius.all(Radius.circular(20))),
         elevation: 20,
       ),
       onPressed: () {
         if (_isLogin) {
-         loguearEstudiante();
+          loguearEstudiante();
         } else {
           Navigator.pushNamed(context, 'registerStudent');
         }
       },
-      child: Column(
-        children: [
-          Text(
-            'Alumno',
-            style: TextStyle(
-              fontSize: 20,
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
+      child: Container(
+        padding: EdgeInsets.only(top: 10, left: 10, right: 10),
+        child: Column(
+          children: [
+            Text(
+              _menuLabel + '\nAlumno',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          ),
-          Image(height: 150, image: AssetImage('assets/images/avatar1.png')),
-        ],
+            Image(height: 150, image: AssetImage('assets/images/avatar1.png')),
+          ],
+        ),
       ),
     );
   }
@@ -192,7 +188,10 @@ class _InitialPageState extends State<InitialPage> {
                 padding: EdgeInsets.all(10),
                 elevation: 10,
               ),
-              child: Icon(Icons.arrow_back,color: kTeacherColor,),
+              child: Icon(
+                Icons.arrow_back,
+                color: kTeacherColor,
+              ),
               onPressed: _isBackDisabled
                   ? null
                   : () => {
@@ -201,8 +200,8 @@ class _InitialPageState extends State<InitialPage> {
                           _backButtonOpacity = 0.0;
                           _isBackDisabled = true;
                           _isButtonDisabled = false;
+                          _logoActual = _logoBlanco;
                           _logoPos = _initLogoPos;
-                          _bottomPos = _initBottomPos;
                           _menuOpacity = 0.0;
                           _menuPosition = -1;
                         })
@@ -216,7 +215,7 @@ class _InitialPageState extends State<InitialPage> {
         alignment: Alignment.center,
         child: Image(
           width: size.width * 0.7,
-          image: AssetImage('assets/images/LogoTemp.png'),
+          image: AssetImage(_logoActual),
         ));
   }
 
@@ -252,14 +251,14 @@ class _InitialPageState extends State<InitialPage> {
           width: size.width,
           alignment: Alignment.center,
           child: ElevatedButton(
-            style: ElevatedButton.styleFrom(primary:Colors.white,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(15)),
-              padding: EdgeInsets.symmetric(horizontal: 80, vertical: 8),
-
-            ),
-            autofocus: true,
-            clipBehavior: Clip.none,
+              style: ElevatedButton.styleFrom(
+                primary: Colors.white,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15)),
+                padding: EdgeInsets.symmetric(horizontal: 80, vertical: 8),
+              ),
+              autofocus: true,
+              clipBehavior: Clip.none,
               onPressed: _isButtonDisabled
                   ? null
                   : () => {
@@ -269,11 +268,11 @@ class _InitialPageState extends State<InitialPage> {
                           _backButtonOpacity = 1.0;
                           _isBackDisabled = false;
                           _isButtonDisabled = true;
+                          _logoActual = _logoNormal;
                           _logoPos = _finalLogoPos;
-                          _bottomPos = _finalBottomPos;
                           _menuOpacity = 1.0;
                           _menuPosition = 0;
-                          _menuLabel = 'Entrar como...';
+                          _menuLabel = 'Entrar como ';
                         })
                       },
               child: Text(
@@ -307,11 +306,11 @@ class _InitialPageState extends State<InitialPage> {
                           _backButtonOpacity = 1.0;
                           _isBackDisabled = false;
                           _isButtonDisabled = true;
+                          _logoActual = _logoNormal;
                           _logoPos = _finalLogoPos;
-                          _bottomPos = _finalBottomPos;
                           _menuOpacity = 1.0;
                           _menuPosition = 0;
-                          _menuLabel = 'Registrarse como...';
+                          _menuLabel = 'Registrarse como ';
                         })
                       },
               child: Text(
@@ -324,52 +323,52 @@ class _InitialPageState extends State<InitialPage> {
               )),
         ));
   }
+
   Student? estudiante;
-  buscarUsuario(){
-    
+  buscarUsuario() {
     final prefs = new PreferenciasUsuario();
-    if(prefs.userId!=''){
+    if (prefs.userId != '') {
       try {
         LoginService loginService = new LoginService();
-        loginService.loginStudent(prefs.userId, prefs.userPasw).then((value){
-        estudiante = value.student;
-        buscando=false;
-        if(intentandoLoguear){
-          loguearEstudiante();
-        }
-      });
+        loginService.loginStudent(prefs.userId, prefs.userPasw).then((value) {
+          estudiante = value.student;
+          buscando = false;
+          if (intentandoLoguear) {
+            loguearEstudiante();
+          }
+        });
       } catch (e) {
         print(e);
         estudiante = null;
         buscando = false;
       }
-    }else{
+    } else {
       estudiante = null;
       buscando = false;
     }
   }
-  loguearEstudiante(){
+
+  loguearEstudiante() {
     usuarioTemporal = Provider.of<ActiveUser>(context, listen: false);
     usuarioTemporal.resultados = [];
     usuarioTemporal.student = estudiante;
     intentandoLoguear = true;
 
-    if(estudiante!=null && !buscando){
-
+    if (estudiante != null && !buscando) {
       if (estudiante!.classgroup != null) {
-          //Busca si el estudiante ya realizo el test inicial, si no lo ha realizado lo manda al test
-          sesionService.getAllSesion(estudiante!.userId!).then((respuesta){
-            if(respuesta.state == 0 && respuesta.sesions!.isEmpty){
-              Navigator.pushReplacementNamed(context, 'initialTest');
-            }else{
-              Navigator.pushNamed(context, 'menuStudent');
-            }
-          });
-        } else {
-          Navigator.pushReplacementNamed(context, 'selectclass');
-        } 
-    }else{
-      intentandoLoguear=false;
+        //Busca si el estudiante ya realizo el test inicial, si no lo ha realizado lo manda al test
+        sesionService.getAllSesion(estudiante!.userId!).then((respuesta) {
+          if (respuesta.state == 0 && respuesta.sesions!.isEmpty) {
+            Navigator.pushReplacementNamed(context, 'initialTest');
+          } else {
+            Navigator.pushNamed(context, 'menuStudent');
+          }
+        });
+      } else {
+        Navigator.pushReplacementNamed(context, 'selectclass');
+      }
+    } else {
+      intentandoLoguear = false;
       Navigator.pushNamed(context, 'loginStudent');
     }
   }
