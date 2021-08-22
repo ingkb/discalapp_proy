0dbfff7c3f10f3e7e5857aa69f6cfb27188dbc38
Dario Ramos <darks1497@gmail.com>
2021-08-21 23:35:15 -0500

    grado

--- lib/pages/Teacher/Clases/add_class_page.dart
+++ lib/pages/Teacher/Clases/add_class_page.dart
@@ -12,7 +12,6 @@ class AddClassgroup extends StatefulWidget {
 }
 
 class _AddClassgroupState extends State<AddClassgroup> {
-
   late Classgroup classgroup;
   late ClassgroupService classesService;
   bool userVerified = false;
@@ -28,105 +27,107 @@ class _AddClassgroupState extends State<AddClassgroup> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: kTeacherColor,
-        title:Text('Crear clase')
-      ),
+      appBar:
+          AppBar(backgroundColor: kTeacherColor, title: Text('Crear clase')),
       body: Center(
-        child:Container(
-          child: ListView(
-             padding: EdgeInsets.symmetric(horizontal: 30, vertical: 30),
-            children: [
-              avatarImage(),
-              SizedBox(height: 20),
-              inputNombre(),
-              
-              SizedBox(height: 20),
-              submitLogin()
-            ],
-          ),
-        )
-      ),
+          child: Container(
+        child: ListView(
+          padding: EdgeInsets.symmetric(horizontal: 30, vertical: 30),
+          children: [
+            avatarImage(),
+            SizedBox(height: 20),
+            inputNombre(),
+            SizedBox(height: 20),
+            inputGrado(),
+            SizedBox(height: 20),
+            submitLogin()
+          ],
+        ),
+      )),
     );
   }
 
- 
-  Widget inputNombre(){
-     return TextField(
+  Widget inputNombre() {
+    return TextField(
       decoration: InputDecoration(
-        focusedBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(20.0),
-          borderSide: const BorderSide(color: kAlumnColor, width: 2.0),
-        ),
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(20.0),
-        ),
-        hintText: 'Nombre',
-        labelText: 'Nombre',
- 
-        suffixIcon: userVerified?Icon(Icons.verified_user):null,
-        icon: Icon(Icons.person)
-      ),
-      onChanged: (valor){
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(20.0),
+            borderSide: const BorderSide(color: kAlumnColor, width: 2.0),
+          ),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          hintText: 'Nombre',
+          labelText: 'Nombre',
+          suffixIcon: userVerified ? Icon(Icons.verified_user) : null,
+          icon: Icon(Icons.person)),
+      onChanged: (valor) {
         setState(() {
-           classgroup.name = valor;
+          classgroup.name = valor;
         });
-      }
-      ,
+      },
     );
   }
 
-   
-  Widget avatarImage(){
+  Widget inputGrado() {
+    return TextField(
+      decoration: InputDecoration(
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(20.0),
+            borderSide: const BorderSide(color: kAlumnColor, width: 2.0),
+          ),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          hintText: 'Grado',
+          labelText: 'Grado',
+          suffixIcon: userVerified ? Icon(Icons.class_) : null,
+          icon: Icon(Icons.class_)),
+      onChanged: (valor) {
+        setState(() {
+          //classgroup.name = valor;
+        });
+      },
+    );
+  }
 
+  Widget avatarImage() {
     return Container(
-      width: 120,
-      height: 120,
-      child: CircleAvatar(
-        child: Image(width: 100,
+        width: 120,
+        height: 120,
+        child: CircleAvatar(
+            child: Image(
+          width: 100,
           image: AssetImage('assets/images/books.png'),
-        ) 
-      )
-    );
+        )));
   }
 
-
- 
-  Widget submitLogin(){
-    return 
-    
-     Container(
-       height: 50,
-       margin: EdgeInsets.symmetric(horizontal: 50),
-       child: ElevatedButton(
-         style: ElevatedButton.styleFrom(
-         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
-         primary: kTeacherColor,
-
-         ),
-          onPressed: (){
+  Widget submitLogin() {
+    return Container(
+      height: 50,
+      margin: EdgeInsets.symmetric(horizontal: 50),
+      child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+            primary: kTeacherColor,
+          ),
+          onPressed: () {
             _registrarClase();
           },
-          child: Text('Crear', style: TextStyle(
-            fontSize: 25,
-            color: Colors.white
-          )
-          )
-        ),
-     )
-      ;
+          child: Text('Crear',
+              style: TextStyle(fontSize: 25, color: Colors.white))),
+    );
   }
 
   _registrarClase() {
     classgroup.teacher = prefs.teacherUserId;
     print('usuario ' + prefs.teacherUserId);
-    classesService.addClassgroup(classgroup).then((value){
-     setState(() {
-       Navigator.pop(context, 'classes');
-      //  Navigator.pop(context);
-     });
-    }
-   );
-        
+    classesService.addClassgroup(classgroup).then((value) {
+      setState(() {
+        Navigator.pop(context, 'classes');
+        //  Navigator.pop(context);
+      });
+    });
   }
 }
