60a8e38925d6a89462290fadb979194c6407a5c7
Dario Ramos <darks1497@gmail.com>
2021-08-25 19:46:24 -0500

    Boton genero

--- lib/pages/Student/register_student_page.dart
+++ lib/pages/Student/register_student_page.dart
@@ -12,6 +12,15 @@ class RegisterStudentPage extends StatefulWidget {
 class _RegisterStudentPageState extends State<RegisterStudentPage> {
   Student? student;
   bool userVerified = false;
+  Color colorOPT1 = kAlumnColor;
+  Color colorOPT2 = kAlumnColor;
+  double borderOPT1 = 1;
+  double borderOPT2 = 1;
+
+  int selectedOption = 3;
+
+  late String imagen1;
+  late String imagen2;
 
   @override
   void initState() {
@@ -21,6 +30,8 @@ class _RegisterStudentPageState extends State<RegisterStudentPage> {
 
   @override
   Widget build(BuildContext context) {
+    imagen1 = 'assets/images/avatar1.png';
+    imagen2 = 'assets/images/avatar2.png';
     return Scaffold(
       appBar: AppBar(
           backgroundColor: kAlumnColor, title: Text('Registro estudiante')),
@@ -29,11 +40,10 @@ class _RegisterStudentPageState extends State<RegisterStudentPage> {
         child: ListView(
           padding: EdgeInsets.symmetric(horizontal: 30, vertical: 30),
           children: [
-            avatarImage(),
+            //avatarImage(),
+            seleccionSexo(),
             SizedBox(height: 20),
             inputEdad(),
-            SizedBox(height: 20),
-            inputGrado(),
             SizedBox(height: 20),
             inputNombre(),
             SizedBox(height: 20),
@@ -89,7 +99,7 @@ class _RegisterStudentPageState extends State<RegisterStudentPage> {
     );
   }
 
-  Widget inputGrado() {
+  /* Widget inputGrado() {
     return TextField(
       keyboardType: TextInputType.number,
       decoration: InputDecoration(
@@ -111,15 +121,77 @@ class _RegisterStudentPageState extends State<RegisterStudentPage> {
       },
     );
   }
+ */
 
-  Widget avatarImage() {
+  Widget seleccionSexo() {
     return Container(
-        width: 200,
-        height: 200,
-        child: CircleAvatar(
-            child: Image(
-          image: AssetImage('assets/images/avatar1.png'),
-        )));
+        width: double.infinity,
+        height: 150,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+                height: 150,
+                width: 150,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(width: borderOPT1, color: colorOPT1)),
+                child: TextButton(
+                  onPressed: () {
+                    setState(() {
+                      seleccionarOPT(1);
+                    });
+                  },
+                  child: CircleAvatar(
+                    child: Image(
+                      fit: BoxFit.fitHeight,
+                      height: 500,
+                      image: AssetImage(imagen1),
+                    ),
+                  ),
+                )),
+            SizedBox(height: 30),
+            Container(
+                height: 150,
+                width: 150,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(width: borderOPT2, color: colorOPT2)),
+                child: TextButton(
+                  onPressed: () {
+                    setState(() {
+                      seleccionarOPT(2);
+                    });
+                  },
+                  child: CircleAvatar(
+                    child: Image(
+                      fit: BoxFit.fill,
+                      height: 200,
+                      image: AssetImage(imagen2),
+                    ),
+                  ),
+                ))
+          ],
+        ));
+  }
+
+  seleccionarOPT(int opt) {
+    switch (opt) {
+      case 1:
+        colorOPT1 = kTeacherColor;
+        borderOPT1 = 5;
+        colorOPT2 = kAlumnColor;
+        borderOPT2 = 1;
+        selectedOption = 1;
+        break;
+      case 2:
+        colorOPT1 = kAlumnColor;
+        borderOPT1 = 1;
+        colorOPT2 = kTeacherColor;
+        borderOPT2 = 5;
+        selectedOption = 2;
+        break;
+    }
   }
 
   Widget nextPage() {
