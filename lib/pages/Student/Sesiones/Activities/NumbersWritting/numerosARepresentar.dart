3e4f9f9797a0ae6b655d027b23392c77bc1db450
Carlos Baquero <carlos.baquero@insoftar.com>
2021-08-29 21:48:51 -0500

    aumento de numeros a respresentar

--- lib/pages/Student/Sesiones/Activities/NumbersWritting/numerosARepresentar.dart
+++ lib/pages/Student/Sesiones/Activities/NumbersWritting/numerosARepresentar.dart
@@ -2,5 +2,7 @@ Map<int,List<String>> numerosARepresentar = {
   0:['908', 'Ochocientos nueve','Novecientos ocho','Novecientos ochenta','2'],
   1:['25', 'Veinticinco','Doce','Veintidos','1'],
   2:['302', 'Trecientos veinte','Trecientos dos', 'Treinta','2'],
-  3:['414','Cuatrocientos cuatro','Cuatro cuarenta', 'Cuatrocientos catorce','3']
+  3:['414','Cuatrocientos cuatro','Cuatro cuarenta', 'Cuatrocientos catorce','3'],
+  4:['213','Docientos trece','Docientos treinta',"Docientos doce",'1'],
+  5:['845','Ochocientos cincuenta y cinco','Ochocientos catorce','Ochocientos cuarenta y cinco','3']
 };
