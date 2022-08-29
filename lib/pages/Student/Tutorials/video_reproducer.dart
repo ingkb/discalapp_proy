26e98a9ba93cbdf79fbcd5762b7771e735492695
Dario <darks1497@gmail.com>
2022-08-28 19:05:45 -0500

    Reproductor listo Falta ponerlo lindo

--- lib/pages/Student/Tutorials/video_reproducer.dart
+++ lib/pages/Student/Tutorials/video_reproducer.dart
@@ -1,5 +1,8 @@
+import 'package:discalapp_proy/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:youtube_player_flutter/youtube_player_flutter.dart';
+
+import '../../../providers/user_preference.dart';
 
 /// Creates list of video players
 class VideoList extends StatefulWidget {
@@ -8,15 +11,15 @@ class VideoList extends StatefulWidget {
 }
 
 class _VideoListState extends State<VideoList> {
+  
   final List<YoutubePlayerController> _controllers = [
-    'gQDByCdjUXw',
-    'iLnmTe5Q2Qw',
-    '_WoCV4c6XOE',
-    'KmzdUe0RSJo',
-    '6jZDSSZZxjQ',
-    'p2lYr3vM_1w',
-    '7QUtEmBT_-w',
-    '34_PXCzGw1M',
+    'VumaYtVeJuc', // sumas
+    'VeyWehidri4', // restas
+    'htgr3pvBr-I', // multiplicacion
+    '4N1iwQxiHrs', // comparacion
+    '_ZyD4n5zqxA', // secuencia
+    'RIyjY7o_yww', // Recta Numerico
+    'CjUVTEExfBg',
   ]
       .map<YoutubePlayerController>(
         (videoId) => YoutubePlayerController(
@@ -31,28 +34,114 @@ class _VideoListState extends State<VideoList> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Video List Demo'),
-      ),
-      body: ListView.separated(
-        itemBuilder: (context, index) {
-          return YoutubePlayer(
-            key: ObjectKey(_controllers[index]),
-            controller: _controllers[index],
-            actionsPadding: const EdgeInsets.only(left: 16.0),
-            bottomActions: [
-              CurrentPosition(),
-              const SizedBox(width: 10.0),
-              ProgressBar(isExpanded: true),
-              const SizedBox(width: 10.0),
-              RemainingDuration(),
-              FullScreenButton(),
-            ],
-          );
-        },
-        itemCount: _controllers.length,
-        separatorBuilder: (context, _) => const SizedBox(height: 10.0),
+      key: _scaffoldKey,
+      endDrawer: drawer(context),
+      body: Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image:
+                      AssetImage('assets/images/Menu_marcoTutorialVacio.jpg'),
+                  fit: BoxFit.fill)),
+          child: ListView(
+            children: [botonesTop(), Reproductor()],
+          )),
+    );
+  }
+
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  Widget Reproductor() {
+    var opcion =  ModalRoute.of(context)!.settings.arguments as int;
+    return Container(
+      margin: EdgeInsets.only(top: 200),
+      child: YoutubePlayer(
+      key: ObjectKey(_controllers[opcion]),
+      controller: _controllers[opcion],
+      actionsPadding: const EdgeInsets.only(left: 16.0),
+      bottomActions: [
+        CurrentPosition(),
+        const SizedBox(width: 10.0),
+        ProgressBar(isExpanded: true),
+        const SizedBox(width: 10.0),
+        RemainingDuration(),
+        FullScreenButton(),
+      ],
+    ));
+  }
+
+  void _openEndDrawer() {
+    _scaffoldKey.currentState!.openEndDrawer();
+  }
+
+  Widget botonesTop() {
+    return Container(
+      margin: EdgeInsets.only(top: 25, right: 15, left: 15, bottom: 20),
+      child: Row(
+        children: [
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+                shape: CircleBorder(), primary: Colors.transparent),
+            child: Container(
+              width: 60,
+              height: 60,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(shape: BoxShape.circle),
+              child: Image(image: AssetImage('assets/images/botonBack.png')),
+            ),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+          Expanded(child: SizedBox()),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+                shape: CircleBorder(), primary: Colors.transparent),
+            child: Container(
+              width: 60,
+              height: 60,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(shape: BoxShape.circle),
+              child: Image(image: AssetImage('assets/images/botonMenu.png')),
+            ),
+            onPressed: () {
+              _openEndDrawer();
+            },
+          ),
+        ],
       ),
     );
+  }
+
+  Drawer drawer(context) {
+    return Drawer(
+        child: ListView(
+      // Important: Remove any padding from the ListView.
+      padding: EdgeInsets.zero,
+      children: <Widget>[
+        DrawerHeader(
+          child: CircleAvatar(
+            child: Image(
+              image: AssetImage('assets/images/avatar1.png'),
+            ),
+          ),
+          decoration: BoxDecoration(
+            color: kAlumnColor,
+          ),
+        ),
+        ListTile(
+            contentPadding: EdgeInsets.only(left: 10),
+            leading: Icon(
+              Icons.exit_to_app,
+              size: 30,
+              color: kAlumnColor,
+            ),
+            title: Text("Cerrar Sesión"),
+            onTap: () {
+              final prefs = new PreferenciasUsuario();
+              prefs.logOutStudent();
+              Navigator.pop(context);
+              Navigator.pushReplacementNamed(context, '/');
+            }),
+      ],
+    ));
   }
 }
