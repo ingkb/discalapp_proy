0d6f70400395d844df4f080b4d0a27f9fd760d39
Dario <darioandres08@hotmail.com>
2022-10-21 18:17:03 -0500

    link de amazon

--- lib/enviroment.dart
+++ lib/enviroment.dart
@@ -1,2 +1,4 @@
-const BaseUrl = 'https://discalproyect.herokuapp.com';
+const BaseUrl = 'http://ec2-3-90-53-56.compute-1.amazonaws.com';
+
+//const BaseUrl = 'https://discalproyect.herokuapp.com';
 //const BaseUrl = 'http://10.0.2.2:3000';
